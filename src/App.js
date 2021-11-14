93d59de0b209ee361ccb144591392cdd7358d940
tiago <tiagoribeiro64625@gmail.com>
2021-11-14 16:47:35 -0500

    Connected forbes 400 api and changed default app page to "FinancePage", look into index.js to see that it by default now calls the FinancePage class in App.js.
    We call the api using the axios library. To install this you will need to run "npm install axios" in your command prompt/terminal. Changed the default App function to a class.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const api = axios.create({
+  baseURL: 'https://forbes400.herokuapp.com/api/forbes400/'
+})
+
+class FinancePage extends Component{
+  state = {
+    billis : [] //Our list of billionaires we will populate from the API
+  }
+
+  constructor() {
+    super();
+    api.get('/').then(res =>{
+      console.log(res.data)
+      this.setState({billis: res.data})
+    })
+  }
+  render(){
+    return <div>
+      <h1>A list of billionaires, descending:</h1>
+      {this.state.billis.map(billis => <h3>{billis.uri}</h3>)}
     </div>
-  );
+  }
 }
 
-export default App;
+export default FinancePage;

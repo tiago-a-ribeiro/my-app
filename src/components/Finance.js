cd5d27c74cf26f67c0ccdbadc733f8d729e139e3
Marco <bozicma@gmail.com>
2021-12-02 14:51:39 -0500

    got all columns to be sortable just need to fix output for net worth change

--- src/components/Finance.js
+++ src/components/Finance.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 import './Finance.css';
+import DataTable from 'react-data-table-component';
 
 function NetWorthChange(props) {
   const difference = ((props.final*1000000)-(props.prev*1000000));
@@ -12,6 +13,103 @@ function NetWorthChange(props) {
   }
   return <td class={colorTag}>${difference.toLocaleString()}</td>;
 }
+
+function sortTableByColumn(table, column, asc = true){
+  const dirModifier = asc ? 1 : -1;
+  const tBody = table.tBodies[0];
+  const rows = Array.from(tBody.querySelectorAll("tr"));
+
+  const sortedRow = rows.sort((a,b) => {
+    const aColText = a.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
+    const bColText = b.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
+    
+    console.log(aColText);
+    console.log(bColText);
+    
+    //console.log(a);
+    //console.log(b);
+  });
+}
+
+const columns = [
+  {
+    name: '',
+    selector: row => row.image,
+    sortable: true,
+},
+  {
+      name: 'Rank',
+      selector: row => row.rank,
+      sortable: true,
+  },
+  {
+      name: 'Name',
+      selector: row => row.name,
+      sortable: true,
+  },
+  {
+    name: 'Live Net Worth',
+    selector: row => row.netWorth,
+    sortable: true,
+},
+{
+  name: 'Previous Day Net Worth',
+  selector: row => row.yesterdayWorth,
+  sortable: true,
+},
+{
+  name: 'Change Compared to Previous Day',
+  selector: row => row.yesterdayWorth,
+  sortable: true,
+},
+{
+  name: 'Private Assets Worth',
+  selector: row => row.privateWorth,
+  sortable: true,
+},
+{
+  name: 'Source',
+  selector: row => row.source,
+  sortable: true,
+},
+{
+  name: 'Country of Residence',
+  selector: row => row.country,
+  sortable: true,
+},
+];
+
+
+function MyComponent(props) {
+  console.log(props.apiData);
+  var data = [];
+  for (let i = 0; i < props.apiData.length; i++) {
+    var person = {
+      id:(i+1), 
+      image:(<img src={props.apiData[i].person.squareImage}></img>),
+      rank:(props.apiData[i].rank),
+      name:(props.apiData[i].person.name),
+      netWorth:((props.apiData[i].finalWorth*1000000).toLocaleString()),
+      yesterdayWorth:((props.apiData[i].estWorthPrev*1000000).toLocaleString()),
+      //<NetWorthChange final={data.finalWorth} prev={data.estWorthPrev}/>
+      //change:(((props.apiData[i].finalWorth)-(props.apiData[i].estWorthPrev)).toLocaleString()),
+      change:(<NetWorthChange final={props.apiData[i].finalWorth} prev={props.apiData[i].estWorthPrev}/>),
+      privateWorth:((props.apiData[i].privateAssetsWorth*1000000).toLocaleString()),
+      source:(props.apiData[i].source),
+      country:(props.apiData[i].countryOfCitizenship)
+    };
+    data.push(person);
+  }
+  return (
+      <DataTable
+          columns={columns}
+          data={data}
+      />
+  );
+};
+
+
+//sortTableByColumn(document.querySelector("table"), 1);
 
 class Finance extends Component {
 
@@ -30,9 +128,9 @@ class Finance extends Component {
       return (
         <div>
           <h1 class="p-2 is-size-3 has-text-weight-bold">Finance Page</h1>
-          
           <div class="table-container p-5">
-            <table class="table is-hoverable">
+          <MyComponent apiData = {this.state.data}/>
+            <table class="table is-hoverable table-sortable">
               <thead class="has-background-light">
                 <tr>
                   <th> </th>
@@ -59,8 +157,6 @@ class Finance extends Component {
                     <td>${(data.privateAssetsWorth*1000000).toLocaleString()}</td>
                     <td>{data.source}</td>
                     <td>{data.countryOfCitizenship}</td>
-                    
-                    
                   </tr>
                 )}
               </tbody>
@@ -68,7 +164,9 @@ class Finance extends Component {
           </div>
         </div>
       );
+      //sortTableByColumn(document.querySelector("table"), 1);
     }
+    //sortTableByColumn(document.querySelector("table"), 1);
 }
-
+//sortTableByColumn(document.querySelector("table"), 1);
 export default Finance;

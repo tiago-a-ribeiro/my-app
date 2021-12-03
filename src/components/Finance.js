cf05732ad1d093456a515bc59f754ddf57673c7e
Marco <bozicma@gmail.com>
2021-12-02 23:01:49 -0500

    added short description to finance page

--- src/components/Finance.js
+++ src/components/Finance.js
@@ -105,9 +105,9 @@ class Finance extends Component {
     
     render() {
       return (
-        <div>
-          
+        <div> 
           <h1 class="p-2 is-size-3 has-text-weight-bold">Finance Page</h1>
+          <h3>&thinsp; This page automatically updates daily to reflect the richest 400 people on Earth. You can find many interesting facts throughout the table and choose to sort some of the columns if you'd like!</h3>
           <div class="table-container p-5">
           <MyComponent apiData = {this.state.data}/>
           </div>

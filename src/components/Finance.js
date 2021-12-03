ee7f4b2716419e744a5cb4c8a731836e500efcfc
Marco <bozicma@gmail.com>
2021-12-02 22:44:21 -0500

    removed second table

--- src/components/Finance.js
+++ src/components/Finance.js
@@ -110,32 +110,16 @@ class Finance extends Component {
           <h1 class="p-2 is-size-3 has-text-weight-bold">Finance Page</h1>
           <div class="table-container p-5">
           <MyComponent apiData = {this.state.data}/>
-            <table class="table is-hoverable table-sortable">
+            <table class="table is-hoverable table-sortable"> 
               <thead class="has-background-light">
                 <tr>
-                  <th> </th>
-                  <th>Rank </th>
-                  <th>Name</th>
-                  <th>Live Net Worth</th>
-                  <th>Previous Day Net Worth</th>
-                  <th>Change Compared to Previous Day</th>
-                  <th>Private Assets Worth</th>
-                  <th>Source</th>
-                  <th>Country of Residence</th>
+                  
                 </tr>
               </thead>
               <tbody>
                 {this.state.data.map(data => 
                   <tr>
-                    <td><img src={data.person.squareImage} width="150" height="150"></img></td>
-                    <td>{data.rank}</td>
-                    <td>{data.person.name}</td>
-                    <td>${(data.finalWorth*1000000).toLocaleString()}</td>
-                    <td>${(data.estWorthPrev*1000000).toLocaleString()}</td>
-                    <NetWorthChange final={data.finalWorth} prev={data.estWorthPrev}/>
-                    <td>${(data.privateAssetsWorth*1000000).toLocaleString()}</td>
-                    <td>{data.source}</td>
-                    <td>{data.countryOfCitizenship}</td>
+                    
                   </tr>
                 )}
               </tbody>
